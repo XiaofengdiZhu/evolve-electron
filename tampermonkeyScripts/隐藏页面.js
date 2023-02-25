7776d91a886b151a65778584e2352eeb777acf7b
销锋镝铸 <523084467@qq.com>
2023-02-25 19:36:15 +0800

    隐藏页面js

---
+++ tampermonkeyScripts/隐藏页面.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         隐藏页面
+// @version      关闭脚本后刷新恢复
+// @author       销锋镝铸
+// ==/UserScript==
+
+window.document.getElementsByTagName("body")[0].style.display = "none"
